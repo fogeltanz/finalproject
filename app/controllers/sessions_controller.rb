e10bc97df24808f639246e4330017e84dd9bdc59
Dustin Greene <dgreene1776@gmail.com>
2014-05-13 19:14:16 -0500

    sign in, no auth

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,28 @@
+class SessionsController < ApplicationController
+
+	def new
+	end
+
+	def create
+		user = User.find_by(email: params[:session][:email].downcase)
+		if user
+			sign_in user 
+			redirect_to user
+		else
+			flash.now[:error] = 'Invalid email/password combination'
+			render 'new'
+		end
+	end
+
+	def destroy
+	end
+
+	private 
+	def sign_in(user)
+		#remember_token = User.new_remember_token
+		#cookies.permanent[:remember_token] = remember_token
+		#user.update_attribute(:remember_token, User.hash(remember_token))
+		#self.current_user = user
+	end
+
+end
